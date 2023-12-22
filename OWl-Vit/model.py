e90e8972f924a94dc3749ba811dee360416ca2cb
Alaaeldinn <ahmad.alaaeldinn@gmail.com>
2023-12-22 15:45:43 +0200

    error handling

--- OWl-Vit/model.py
+++ OWl-Vit/model.py
@@ -9,7 +9,7 @@ class ObjectDetector:
         self.processor = OwlViTProcessor.from_pretrained(model_path)
         self.model = OwlViTForObjectDetection.from_pretrained(model_path)
         self.threshold = threshold
-        self.image_array = None
+        self.image_tensor = None
         self.outputs = None
         self.boxes = None
         self.scores = None
@@ -18,41 +18,34 @@ class ObjectDetector:
 
     def load_image(self, image_path):
         try:
-            image = Image.open(image_path)
-            self.image_array = torch.tensor(image).to(torch.float32) / 255.0
-            self.image_array = self.image_array.unsqueeze(0)
-            image.close()
-            torch.cuda.empty_cache()
+            with Image.open(image_path) as image:
+                self.image_tensor = torch.tensor(image).to(torch.float32) / 255.0
+                self.image_tensor = self.image_tensor.unsqueeze(0)
         except Exception as e:
-            print(f"Error loading image: {e}")
-            exit(1)
+            raise ValueError(f"Error loading image: {e}")
 
     def inference(self, texts):
         try:
             with torch.no_grad():
-                inputs = self.processor(text=texts, images=self.image_array, return_tensors="pt")
+                inputs = self.processor(text=texts, images=self.image_tensor, return_tensors="pt")
                 self.outputs = self.model(**inputs)
-            torch.cuda.empty_cache()
         except Exception as e:
-            print(f"Error during inference: {e}")
-            exit(1)
+            raise RuntimeError(f"Error during inference: {e}")
 
-    def post_process(self):
-        target_sizes = torch.Tensor([self.image_array.size()[2:]])
+    def post_process(self, texts):
+        target_sizes = torch.Tensor([self.image_tensor.size()[2:]])
         try:
             results = self.processor.post_process_object_detection(outputs=self.outputs, target_sizes=target_sizes, threshold=self.threshold)
             i = 0
             self.text = texts[i]
             self.boxes, self.scores, self.labels = results[i]["boxes"], results[i]["scores"], results[i]["labels"]
             del results  # Remove unused variables to release memory
-            torch.cuda.empty_cache()
         except Exception as e:
-            print(f"Error during post-processing: {e}")
-            exit(1)
+            raise RuntimeError(f"Error during post-processing: {e}")
 
     def plot_boxes(self):
         fig, ax = plt.subplots(1)
-        ax.imshow(self.image_array.squeeze(0).permute(1, 2, 0).numpy())
+        ax.imshow(self.image_tensor.squeeze(0).permute(1, 2, 0).numpy())
         for box, score, label in zip(self.boxes, self.scores, self.labels):
             box = [round(i, 2) for i in box.tolist()]
             rect = patches.Rectangle((box[0], box[1]), box[2] - box[0], box[3] - box[1], linewidth=2, edgecolor="r", facecolor="none")
@@ -69,5 +62,5 @@ if __name__ == "__main__":
     object_detector = ObjectDetector()
     object_detector.load_image(image_path)
     object_detector.inference(texts)
-    object_detector.post_process()
+    object_detector.post_process(texts)
     object_detector.plot_boxes()

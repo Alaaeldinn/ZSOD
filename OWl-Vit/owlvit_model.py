6916173ed74ef331600e3ffa4897004cf8843545
Alaaeldinn <ahmad.alaaeldinn@gmail.com>
2023-12-22 19:34:52 +0200

    fixed

--- OWl-Vit/owlvit_model.py
+++ OWl-Vit/owlvit_model.py
@@ -1,7 +1,6 @@
 from PIL import Image
 import torch
 from transformers import OwlViTProcessor, OwlViTForObjectDetection
-from plotter import plot_boxes
 
 class OWLVIT:
     def __init__(self, model_path="google/owlvit-base-patch32", threshold=0.1):
@@ -42,16 +41,4 @@ class OWLVIT:
             return result[0]["boxes"], result[0]["scores"], result[0]["labels"], self.text
         except Exception as e:
             raise RuntimeError(f"Error during post-processing: {e}")
-
-if __name__ == "__main__":
-    # Load the image directly as a PIL Image
-    image_path = '/content/s-l1200.jpg'
-    image = Image.open(image_path)
-
-    prompts = [["a photo of all txt"]]
-
-    model = OWLVIT()
-    boxes, scores, labels, text = model(image, prompts)
-
-    # Use the plot function outside the class
-    plot_boxes(image, boxes, scores, labels, text)
+        
